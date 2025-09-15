50ff4d8361f12fb960d5193189339c081b53d2e7
Sankalp <sankalp13353@gmail.com>
2025-09-15 22:57:49 +0530

    About Section

---
+++ src/components/About.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import "../Navbar.css"; 
+
+const About = () => {
+  return (
+    <section className="about" id="about">
+      <h2>About Me</h2>
+      <p>
+        Hi! I’m Sankalp, a passionate frontend developer and competitive
+        programmer. I enjoy building interactive web apps and solving
+        challenging problems on coding platforms.
+      </p>
+
+      <div className="skills">
+        <h3>Skills</h3>
+        <ul>
+          <li>React.js</li>
+          <li>JavaScript (ES6+)</li>
+          <li>HTML5 & CSS3</li>
+          <li>Python</li>
+          <li>Data Structures & Algorithms</li>
+          <li>Git & GitHub</li>
+        </ul>
+      </div>
+
+      <div className="coding-stats">
+        <h3>Competitive Coding</h3>
+        <div className="platforms">
+          <div className="platform-card">
+            <h4>LeetCode</h4>
+            <p>Rating: 1800</p>
+            <p>Questions Solved: 250+</p>
+          </div>
+          <div className="platform-card">
+            <h4>Codeforces</h4>
+            <p>Rating: 1600</p>
+            <p>Problems Solved: 300+</p>
+          </div>
+          <div className="platform-card">
+            <h4>HackerRank</h4>
+            <p>Rank: 5⭐ (Problem Solving)</p>
+            <p>Challenges Solved: 200+</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,6 @@ const Navbar = () => {
 
 
       <ul className="navbar-links">
-        <li><a href="#home">Home</a></li>
         <li><a href="#about">About</a></li>
         <li><a href="#projects">Projects</a></li>
         <li><a href="#internships">Experience</a></li>
@@ -19,4 +18,4 @@ const Navbar = () => {
   );
 };
 
-export default Navbar;
+export default Navbar
