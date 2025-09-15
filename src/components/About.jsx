b123040641784b6f1d05acd2a9ed4d0566b35603
Sankalp <sankalp13353@gmail.com>
2025-09-15 23:08:25 +0530

    about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,46 +1,35 @@
 import React from "react";
-import "../About.css"; 
+import "../About.css";
 
 const About = () => {
   return (
     <section className="about" id="about">
-      <h2>About Me</h2>
-      <p>
-        Hi! I’m Sankalp, a passionate frontend developer and competitive
-        programmer. I enjoy building interactive web apps and solving
-        challenging problems on coding platforms.
-      </p>
+      <div className="about-content">
+        <div className="about-left">
+          <p>
+            Hi! I’m Sankalp, a passionate frontend developer and competitive
+            programmer. I enjoy building interactive web apps and solving
+            challenging problems on coding platforms.
+          </p>
 
-      <div className="skills">
-        <h3>Skills</h3>
-        <ul>
-          <li>React.js</li>
-          <li>JavaScript (ES6+)</li>
-          <li>HTML5 & CSS3</li>
-          <li>Python</li>
-          <li>Data Structures & Algorithms</li>
-          <li>Git & GitHub</li>
-        </ul>
-      </div>
+          <div className="coding-stats">
+            <h3>Competitive Coding</h3>
+            <div className="platforms">
+              <div className="platform-card">
+                <h4>LeetCode</h4>
+                <p>Rating: 1350+</p>
+                <p>Questions Solved: 120+</p>
+              </div>
+              <div className="platform-card">
+                <h4>Codeforces</h4>
+                <p>Rating: 800+</p>
+              </div>
+            </div>
+          </div>
+        </div>
 
-      <div className="coding-stats">
-        <h3>Competitive Coding</h3>
-        <div className="platforms">
-          <div className="platform-card">
-            <h4>LeetCode</h4>
-            <p>Rating: 1800</p>
-            <p>Questions Solved: 250+</p>
-          </div>
-          <div className="platform-card">
-            <h4>Codeforces</h4>
-            <p>Rating: 1600</p>
-            <p>Problems Solved: 300+</p>
-          </div>
-          <div className="platform-card">
-            <h4>HackerRank</h4>
-            <p>Rank: 5⭐ (Problem Solving)</p>
-            <p>Challenges Solved: 200+</p>
-          </div>
+        <div className="about-right">
+          <img src="/profile.jpg" alt="Sankalp" className="profile-img" />
         </div>
       </div>
     </section>
